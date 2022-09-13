97eca9566b0a9f569d9c99a679b1c8a271a2b860
Abduino <abdrehmanredi0@gmail.com>
2022-09-13 04:33:10 -0300

    header added

---
+++ onlyArduino.ino
@@ -0,0 +1,32 @@
+#include<dht.h>
+#include <Adafruit_BMP085.h>
+Adafruit_BMP085 bmp;
+
+
+#define DHT11_PIN 2
+dht DHT;
+
+int moisture_sensor_pin = A0;
+int light_sensor_pin = A1;
+int gas_sensor_pin = A2;
+
+//int moisture_sensor_pin = A0;
+ 
+float moisture_sensor_value=10.0;
+float temperature_sensor_value=10.0;
+float humidity_sensor_value=10.0;
+float light_sensor_value=10.0;
+
+float gas_sensor_value;
+
+float air_pressure_sensor_value=10;
+float sealevel_pressure_sensor_value=10;
+float altitude_sensor_value=10;
+float altitude_sensor_value_2=10;
+
+
+void loop() 
+{
+  Serial.println();
+  delay(5000);
+}
